9e2fbe5a7192fec3404d31a2b3459db02a51b00e
Jackson Tian <shyvo1987@gmail.com>
2017-03-09 17:35:52 +0800

    Dingbot generator (#1)
    * Bump 1.1.0
    
    * Add demo.js

--- index.js
+++ index.js
@@ -7,41 +7,43 @@ class Bot {
     this.webhook = webhook;
   }
 
-  async send(data) {
-    var response = await httpx.request(this.webhook, {
+  send(content) {
+    return httpx.request(this.webhook, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      data: JSON.stringify(data)
+      data: JSON.stringify(content)
+    }).then((response) => {
+      return httpx.read(response, 'utf8');
+    }).then((data) => {
+      return JSON.parse(data);
     });
-    var data = await httpx.read(response, 'utf8');
-    return JSON.parse(data);
   }
 
   text(content, at = {}) {
     return this.send({
-      "msgtype": "text",
-      "text": {
-        "content": content
+      'msgtype': 'text',
+      'text': {
+        'content': content
       },
-      "at": at
+      'at': at
     });
   }
 
   link(link) {
     return this.send({
-      "msgtype": "link",
-      "link": link
+      'msgtype': 'link',
+      'link': link
     });
   }
 
   markdown(title, text) {
     return this.send({
-     "msgtype": "markdown",
-     "markdown": {
-        "title": title,
-        "text": text
+      'msgtype': 'markdown',
+      'markdown': {
+        'title': title,
+        'text': text
       }
     });
   }
